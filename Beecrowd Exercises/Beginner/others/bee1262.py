499c773e71a9f2484e281c63ad5d7d8b5e51b7fb
Gabriel Borges <devgabrielsborges@proton.me>
2024-03-15 00:02:30 -0300

    rtry

--- Beecrowd Exercises/Beginner/others/bee1262.py
+++ Beecrowd Exercises/Beginner/others/bee1262.py
@@ -10,24 +10,24 @@
 
 entrada = str(input())
 limite_r = int(input())
-n_operacoes = entrada.count("W")
-
-if "R" in entrada:
-    if entrada.count("R") % limite_r != 0:
-        n_operacoes += entrada.count("R") // limite_r + 1
-    else:
-        n_operacoes += entrada.count("R") / limite_r
+n_operacoes, numero_rs = 0, 0
 
 for p, v in enumerate(entrada):
+    print(p == int())
     print(f"{p}o --> {v}")
-    if v == "R" and entrada[p + 1] == "R":
-        if numero_rs < limite_r:
+    prox = int(p + 1 if (p + 1) < len(entrada) else p)   # proximo valor recebe p + 1 se não for o último valor, senão --> p = p
+    print(f"prox = int? --> {prox == int()} --- prox = {prox} --> {type(prox)}")
+    if v == "R" and entrada[prox] == "R":
+        if numero_rs < limite_r and prox != len(entrada):
             numero_rs += 1
+            print(f"N Rs --> {numero_rs}")
         else:
+            print("Limite alcançado")
             n_operacoes += 1
             numero_rs = 0
     else:
+        print("Foi W")
         n_operacoes += 1
-
-print(n_operacoes)
+        numero_rs = 0
+    print(n_operacoes + 1)
 
